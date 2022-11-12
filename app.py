a33789b4f1bd1f10d95aa707294644ee6d4fc79e
FreddyxD5 <freddyxd5@gmail.com>
2022-11-11 22:32:27 -0500

    user routes add +  template base + databse config

---
+++ app.py
@@ -0,0 +1,14 @@
+# from flask import Flask, request
+# from markupsafe import escape
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def hello():
+#     name = request.args.get("name", "de mar")
+#     return f'Holitas de {escape(name)}'
+#
+# @app.route('/holitas')
+# def holitas():
+#     name = request.args.get("name", "de mar")
+#     return f'Siuuuuuuuuuuu de {escape(name)}'
